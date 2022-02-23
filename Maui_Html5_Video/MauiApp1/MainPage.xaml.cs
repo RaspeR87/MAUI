a2555591c9d8d0e44b94ae10ee418d746d3f6b0a
RaspeR87 <rasper87@outlook.com>
2022-02-23 13:38:52 +0100

    Add files via upload

---
+++ Maui_Html5_Video/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		var htmlSource = new HtmlWebViewSource();
+		htmlSource.Html = @"<html><body><video autoplay muted loop playsinline class='slideContent' style='width:100%'><source src='https://dev1.kompas-xnet.si/TRIMOTERM.mp4' type='video/mp4'></video></body></html>";
+		webView.Source = htmlSource;
+	}
+}
+
